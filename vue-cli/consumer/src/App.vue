265063275c0389f580d0bcb393b1ba04210d4a6a
tzuchienkao <oyan114@gmail.com>
2021-10-28 16:00:28 +0800

    module federation vue-cli

---
+++ vue-cli/consumer/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <h4>
+      &lt;start&gt; 我是組合引用的版 &lt;/start&gt;
+    </h4>
+    <Section>
+      <Button>我是引用被暴露的按鈕</Button>
+      <br />
+    </Section>
+    <p>
+      &lt;end&gt; 我是組合引用的版 &lt;/end&gt;
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {
+    Button: () => import("core/Button"),
+    Section: () => import("core/Section"),
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-cli/core/src/components/Section.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="section">
+    <h1>
+      嗨～我是被暴露的組件
+    </h1>
+    <slot />
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Section',
+}
+</script>
+
+<style>
+.section {
+  border: 1px solid black;
+  padding: 10px;
+  background-color: rgba(0,0,0,0.3);
+}
+</style>
